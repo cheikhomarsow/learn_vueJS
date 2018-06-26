24b4ae13523ec6feef9f58f09f4f59dfbe20be84
cheikhomarsow <cheikh-omar.sow@supinternet.fr>
2018-06-26 23:12:06 +0200

    component communication example 2

--- component/component_communication_example_2/main.js
+++ component/component_communication_example_2/main.js
@@ -1,13 +1,24 @@
+window.Event = new class {
+    constructor(){
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null){
+        this.vue.$emit(event , data);
+    }
+
+    listen(event, callback){
+        this.vue.$on(event, callback);
+    }
+}
 Vue.component('coupon' , {
     template: "<input placeHolder='Enter your coupon code' @blur=\"onCouponApplied\">",
-
     methods: {
         onCouponApplied() {
-            this.$emit('applied')
+            Event.fire('applied');
         }
     }
 });
-
 
 new Vue({
     el: '#root',
@@ -16,10 +27,8 @@ new Vue({
             couponApplied: false
         }
     },
-    methods: {
-        onCouponApplied() {
-            this.couponApplied = true
-        }
+    created() {
+        Event.listen('applied', () => alert('Handing it!'));
     }
 });
 
