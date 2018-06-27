ddeeee0d188f9bca253e84ce6861116b1d6534fa
cheikhomarsow <cheikh-omar.sow@supinternet.fr>
2018-06-27 22:41:10 +0200

    ajax requests with axios

---
+++ Webpack_and_VueCli/my-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <message>Hello There</message>
+      <message>Hello There 2</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+export default {
+  name: 'app',
+  components: {
+    Message
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Webpack_and_VueCli/my-app/src/components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box is-info">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+    }
+</script>
+
+<style>
+.box{
+  background: gainsboro;
+  color: red;
+  padding: 10px;
+}
+</style>
